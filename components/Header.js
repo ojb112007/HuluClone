3dd21051bbac65a00520d27241f7dd2de699bfbf
Olivier <ojb112007@hotmail.com>
2025-11-29 13:51:40 -0600

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Row } from 'reactstrap'
+import Image from "next/image"
+import { HomeIcon, CheckBadgeIcon, MagnifyingGlassIcon, RectangleStackIcon, BoltIcon, UserIcon } from '@heroicons/react/24/outline'
+import HeaderItem from './HeaderItem'
+
+function Header() {
+  return (
+    <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+        <div className='flex flex-grow justify-evenly max-w-2xl'>
+            <HeaderItem title = 'HOME' Icon ={HomeIcon}/>
+            <HeaderItem title = 'TRENDING' Icon ={BoltIcon}/>
+            <HeaderItem title = 'VERIFIED' Icon ={CheckBadgeIcon}/>
+            <HeaderItem title = 'COLLECTIONS' Icon ={RectangleStackIcon}/>
+            <HeaderItem title = 'SEARCH' Icon ={MagnifyingGlassIcon}/>
+            <HeaderItem title = 'ACCOUNT' Icon ={UserIcon}/>
+        </div>
+        <Image 
+        className='object-contain'
+        src="/hulu-logo.png"
+        alt="Hulu"
+        width={200}
+        height={100}/>
+    </header>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px"
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
+}
